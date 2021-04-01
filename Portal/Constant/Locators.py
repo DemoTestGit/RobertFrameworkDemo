4e3c975d7d209a7b953b1f79ad49ed67851f4cde
Automation user 1 <mythilicool@gmail.com>
2021-04-01 09:04:16 +0530

    Updated

--- Portal/Constant/Locators.py
+++ Portal/Constant/Locators.py
@@ -4,4 +4,18 @@ PwdId="id:txtPassword"
 
 LoginBtn="id:btnLogin"
 
+ClickAdmin = "xpath://a[@id='menu_admin_viewAdminModule']"
+
+username_Delete = "id:searchSystemUser_userName"
+
+search_Delete = "id:searchBtn"
+
+checkbox_delete = "xpath://table[@id='resultTable']/tbody/tr/td/input"
+
+deleteButton = "id:btnDelete"
+
+confirmButton = "id:dialogDeleteBtn"
+
+
+
 txtError= "xpath://*[@id='spanMessage']"
